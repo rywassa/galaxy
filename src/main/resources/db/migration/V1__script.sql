fe56194dcd9e1a162e9c5a99b368a37e0cd366f0
Rogerio Ywassa <rogerio.ywassa@navita.com.br>
2020-05-17 21:25:09 -0300

    Galaxy Forecast Challenge + Improved flyway script.

--- src/main/resources/db/migration/V1__script.sql
+++ src/main/resources/db/migration/V1__script.sql
@@ -1,4 +1,4 @@
-create table if not exists planet (
+create table planet (
 	id bigserial,
 	name varchar(255) not null,
 	angular_speed_in_degree_by_day numeric(19,6) not null,
@@ -8,7 +8,7 @@ create table if not exists planet (
 	constraint planet_pk primary key (id)
 );
 
-create table if not exists galaxy (
+create table galaxy (
 	id serial,
 	name varchar(255) not null,
 	origin_date date not null,
@@ -22,7 +22,7 @@ create table if not exists galaxy (
 	constraint galaxy_planet_three_fk foreign key (planet_three_id) references planet(id)
 );
 
-create table if not exists weather (
+create table weather (
 	id bigserial,
 	day bigint not null,
 	forecast varchar(255) not null,
@@ -31,10 +31,14 @@ create table if not exists weather (
 	constraint weather_galaxy_fk foreign key (galaxy_id) references galaxy(id)
 );
 
-create unique index if not exists weather_galaxy_id_day_uk on weather(galaxy_id, day);
+create unique index weather_galaxy_id_day_uk on weather(galaxy_id, day);
 
 insert into planet(id, name, angular_speed_in_degree_by_day, clockwise, distance_in_km, last_angle_in_rad) values(1, 'Ferengi', 1, true, 500, 0);
 insert into planet(id, name, angular_speed_in_degree_by_day, clockwise, distance_in_km, last_angle_in_rad) values(2, 'Betasoide', 3, true, 2000, 0);
 insert into planet(id, name, angular_speed_in_degree_by_day, clockwise, distance_in_km, last_angle_in_rad) values(3, 'Vulcano', 5, false, 1000, 0);
 
 insert into galaxy(id, name, origin_date, day, planet_one_id, planet_two_id, planet_three_id) values(1, 'Galaxy M', '2020-05-17', 0, 1, 2, 3);
+
+select nextval('planet_id_seq');
+select nextval('planet_id_seq');
+select nextval('planet_id_seq');
